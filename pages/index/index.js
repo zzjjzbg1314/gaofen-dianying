175420d2c65fd4182e625648b7e7e9d7ac7cf715
zongjie.zheng <zhengzongjie@meituan.com>
2017-06-30 02:09:02 +0800

    fix

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,10 +6,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    welcome_list:[1,2,3,4,6],
-    hotmovies_2016:[],
+    welcome_list: [1, 2, 3, 4, 6],
+    hotmovies_2016: [],
     hotmovies_2017: [],
-    hotmovies_douban_top:[],
+    hotmovies_douban_top: [],
     hiddenLoading: false
   },
 
@@ -17,24 +17,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that =this;
+    var that = this;
     var dataUrl = app.globalData.dianyingBase + "queryMoviesByType";
-    that.queryMoviesByType(dataUrl, "103", "hotmovies_2017", "2017豆瓣年度最热")
+    that.queryMoviesByType(dataUrl,["102","103","104"]);
 
-    that.queryMoviesByType(dataUrl, "102", "hotmovies_2016", "2016豆瓣年度最热")
-
-    that.queryMoviesByType(dataUrl, "104", "hotmovies_douban_top", "豆瓣top50")
-
-    this.setData({
-      hiddenLoading: true
-    })
+    
   },
-  queryMoviesByType: function (dataUrl, typeStr, categoryDatas, categoryTitle){
-    var that=this;
+  onReady: function () {
+
+  },
+  onShow: function () {
+     
+  },
+  queryMoviesByType: function (dataUrl, typeListStrs) {
+    var that = this;
     wx.request({
       url: dataUrl,
       data: {
-        typeStr: typeStr,
+        typeListStrs: typeListStrs,
         welcomeFlag: "YES"
       },
       method: 'POST',
@@ -43,16 +43,36 @@ Page({
       },
       success: function (res) {
         var readyData = {};
-        readyData[categoryDatas] = {
-          categoryTitle: categoryTitle,
-          movies: res.data.data
+        console.log(res.data.data)
+        var allMovies = res.data.data;
+
+        readyData["hotmovies_2017"] = {
+          categoryTitle: "2017豆瓣年度最热",
+          movies: allMovies[102]
         }
         that.setData(readyData);
+
+        readyData["hotmovies_2016"] = {
+          categoryTitle: "2016豆瓣年度最热",
+          movies: allMovies[103]
+        }
+        that.setData(readyData);
+
+        readyData["hotmovies_douban_top"] = {
+          categoryTitle: "豆瓣top50",
+          movies: allMovies[104]
+        }
+        that.setData(readyData);
+        
+        that.setData({
+          hiddenLoading: true
+        })
       },
       fail: function (error) {
         console.log(error.msg);
       }
     });
+
   },
 
 
